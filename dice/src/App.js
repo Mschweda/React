fb3beb3ce1d3e1f821aa7c2ef2f85b565f0d662a
Mschweda <tahoe27@gmail.com>
2019-08-05 16:16:04 -0500

    dice app

---
+++ dice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RollDie from './RollDice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <RollDie />
+     
+    </div>
+  );
+}
+
+export default App;
